b36f7c0746db41a91d11555f5994a3bfeb0c75b5
Samuel Lim <samuel.lim23@d155.org>
2022-10-10 13:53:53 -0500

    tap gesture done

--- GestureRecognizerMiniApp/ViewController.swift
+++ GestureRecognizerMiniApp/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var imagePlate: UIImageView!
+    
+    @IBOutlet weak var imageWaffle: UIImageView!
+    @IBOutlet weak var imageBacon: UIImageView!
+    @IBOutlet weak var imageEgg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func actionTapGesture(_ sender: UITapGestureRecognizer) {
+        let tapLoc = sender.location(in: view)
+        imagePlate.center = tapLoc
+    }
+    
+    
+    
+    @IBAction func panView(_ sender: UIPanGestureRecognizer) {
+        let panLoc = sender.location(in: view)
+    sender.center = panLoc
+     
+    }
+    
+    
 }
 
